1e1a64aa1774ec909d5c59ef809a4d388ef67674
thatsongsucks <andrew.tisdall@gmail.com>
2022-12-19 21:02:13 -0800

    tinkering

--- src/routes/signDocuments.js
+++ src/routes/signDocuments.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const axios = require('axios');
 
 const { currentUser } = require('../middlewares/current-user.js');
 const { requireAuth } = require('../middlewares/require-auth.js');
 const sendEnvelope = require('../services/docusign/sendEnvelope');
 const getDSAuthCode = require('../services/docusign/getDSAuthCode');
+const { DOCUSIGN_ACCOUNT_ID } = require('../keys');
 
 const router = express.Router();
 
@@ -19,12 +21,21 @@ router.post('/docusign/sign', async (req, res) => {
     signerName: 'Andrew Tisdall',
     signerEmail: 'andy@ckoakland.org',
     signerClientId: '5',
-    dsReturnUrl: 'http://localhost:3000/onboarding/documents',
+    dsReturnUrl: 'http://localhost:3000/onboarding/docusign/success',
     authCode,
   };
 
   const results = await sendEnvelope(envelopeArgs);
   res.send(results);
+});
+
+router.get('/docusign/getDoc/:envelopeId', async (req, res) => {
+  const { envelopeId } = req.params;
+  const BASE_PATH = 'https://demo.docusign.net/restapi';
+  const docs = axios.get(
+    `${BASE_PATH}/v2/accounts/${DOCUSIGN_ACCOUNT_ID}/envelopes/${envelopeId}/documents/combined`
+  );
+  res.send(docs);
 });
 
 module.exports = router;
--- src/services/docusign/sendEnvelope.js
+++ src/services/docusign/sendEnvelope.js
@@ -6,10 +6,7 @@ const makeEnvelope = require('./createEnvelope');
 const makeRecipientViewRequest = require('./createView');
 
 module.exports = async (args) => {
-  let dsApiClient = new docusign.ApiClient({
-    // oAuthBasePath: 'account-d.docusign.com',
-    // basePath: 'demo.docusign.net/restapi',
-  });
+  let dsApiClient = new docusign.ApiClient();
   dsApiClient.setOAuthBasePath('account-d.docusign.com');
 
   const { DOCUSIGN_ACCOUNT_ID, DOCUSIGN_ID, DOCUSIGN_SECRET } =
@@ -20,69 +17,38 @@ module.exports = async (args) => {
     DOCUSIGN_SECRET,
     args.authCode
   );
-  // console.log(token.accessToken);
-  dsApiClient.setBasePath('demo.docusign.net/restapi');
+
+  // dsApiClient.setBasePath('demo.docusign.net/restapi');
   // const userInfo = await dsApiClient.getUserInfo(token.accessToken);
   // dsApiClient.setBasePath(userInfo.accounts[0].baseUri + '/restapi');
-
-  dsApiClient.addDefaultHeader('Authorization', 'Bearer ' + token.accessToken);
-  // console.log(userInfo.accounts[0]);
-
-  let envelopesApi = new docusign.EnvelopesApi(dsApiClient),
-    results = null;
 
   // Make the envelope request body
   let envelope = makeEnvelope(args);
 
   // Call Envelopes::create API method
-  // Exceptions will be caught by the calling function
+  const BASE_PATH = 'https://demo.docusign.net/restapi';
+  const headers = {
+    Authorization: `Bearer ${token.accessToken}`,
+    'Content-Type': 'application/json',
+  };
 
   const res = await axios.post(
-    `https://demo.docusign.net/restapi/v2/accounts/${DOCUSIGN_ACCOUNT_ID}/envelopes`,
+    `${BASE_PATH}/v2/accounts/${DOCUSIGN_ACCOUNT_ID}/envelopes`,
     envelope,
-    {
-      headers: {
-        Authorization: `Bearer ${token.accessToken}`,
-        'Content-Type': 'application/json',
-      },
-    }
+    { headers }
   );
 
   const { envelopeId } = res.data;
-  // envelopesApi.createEnvelope(
-  //   DOCUSIGN_ACCOUNT_ID,
-  //   {
-  //     envelopeDefinition: envelope,
-  //   },
-  //   async (err, data, response) => {
-  //     // let envelopeId = results.envelopeId;
-  //     // console.log(`Envelope was created. EnvelopeId ${envelopeId}`);
-  //     if (err) {
-  //       console.log(err);
-  //     }
-  //     console.log(data, response);
+
   // Step 3. create the recipient view, the embedded signing
-  let viewRequest = makeRecipientViewRequest(args);
+  let viewRequest = makeRecipientViewRequest({ ...args, envelopeId });
+
   // // Call the CreateRecipientView API
-  // // Exceptions will be caught by the calling function
-  // results = await envelopesApi.createRecipientView(
-  //   DOCUSIGN_ACCOUNT_ID,
-  //   res.data.envelopeId,
-  //   {
-  //     recipientViewRequest: viewRequest,
-  //   }
-  // );
   const result = await axios.post(
-    `https://demo.docusign.net/restapi/v2/accounts/${DOCUSIGN_ACCOUNT_ID}/envelopes/${envelopeId}/views/recipient`,
+    `${BASE_PATH}/v2/accounts/${DOCUSIGN_ACCOUNT_ID}/envelopes/${envelopeId}/views/recipient`,
     viewRequest,
-    {
-      headers: {
-        Authorization: `Bearer ${token.accessToken}`,
-        'Content-Type': 'application/json',
-      },
-    }
+    { headers }
   );
-  // console.log(result);
 
   return result.data.url;
 };
