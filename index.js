e01db056eaf54b40b2f087c0a1468224895f7f99
thatsongsucks <andrew.tisdall@gmail.com>
2023-01-25 20:24:22 -0800

    try to serve static correctly

--- index.js
+++ index.js
@@ -43,7 +43,8 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 const root = path.join(__dirname, 'client', 'build');
-app.use(express.static(root));
+app.use(express.static(path.join(root, 'static')));
+app.use(express.static(path.join(root, 'images')));
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
