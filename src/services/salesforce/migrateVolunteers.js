7817f8952a17d87b4c5550ff86bd07a265658731
thatsongsucks <andrew.tisdall@gmail.com>
2023-01-26 15:55:34 -0800

    script to fix home chef invite whoopsie

--- src/services/salesforce/migrateVolunteers.js
+++ src/services/salesforce/migrateVolunteers.js
@@ -60,4 +60,100 @@ const getContacts = async () => {
   }
 };
 
-module.exports = migrate;
+const updateContactsToInvited = async () => {
+  const contacts = [
+    '0038Z000035F5YvQAK',
+    '0038Z000035IGpwQAG',
+    '0038Z000035IBTxQAO',
+    '0038Z000035IBTyQAO',
+    '0038Z000035IBTzQAO',
+    '0038Z000035IBU0QAO',
+    '0038Z000035IBU1QAO',
+    '0038Z000035IBU2QAO',
+    '0038Z000035IBU3QAO',
+    '0038Z000035IBU4QAO',
+    '0038Z000035IBU5QAO',
+    '0038Z000035IBU6QAO',
+    '0038Z000035IBU7QAO',
+    '0038Z000035IBU8QAO',
+    '0038Z000035IBU9QAO',
+    '0038Z000035IBUAQA4',
+    '0038Z000035IBUBQA4',
+    '0038Z000035IBUCQA4',
+    '0038Z000035IBUDQA4',
+    '0038Z000035IBUEQA4',
+    '0038Z000035IBUFQA4',
+    '0038Z000035IBUGQA4',
+    '0038Z000035IBUHQA4',
+    '0038Z00002z1r9yQAA',
+    '0038Z00002z1r9zQAA',
+    '0038Z00002z2QJ3QAM',
+    '0038Z000035Gsz0QAC',
+    '0038Z000035GszjQAC',
+    '0038Z000035Gsy7QAC',
+    '0038Z000035GuaPQAS',
+    '0038Z000035GKPyQAO',
+    '0038Z000035GKUPQA4',
+    '0038Z000035GLDkQAO',
+    '0038Z000035GKu1QAG',
+    '0038Z000035GKu2QAG',
+    '0038Z000035GKu3QAG',
+    '0038Z000035GKv8QAG',
+    '0038Z000035GKuDQAW',
+    '0038Z000035GKu4QAG',
+    '0038Z000035GKu5QAG',
+    '0038Z000035GKu6QAG',
+    '0038Z000035GKu7QAG',
+    '0038Z000035GKu8QAG',
+    '0038Z000035GKu9QAG',
+    '0038Z000035GKuAQAW',
+    '0038Z000035GKuBQAW',
+    '0038Z000035GKuCQAW',
+    '0038Z00002z3DeJQAU',
+    '0038Z000035HOIMQA4',
+    '0038Z000030UTrhQAG',
+    '0038Z000030UYB1QAO',
+    '0038Z000030UY9sQAG',
+    '0038Z000030UYCZQA4',
+    '0038Z000030UYCYQA4',
+    '0038Z000035GKPjQAO',
+    '0038Z000032klx1QAA',
+    '0038Z000035GL1eQAG',
+    '0038Z000030Unt4QAC',
+    '0038Z000030Us0cQAC',
+    '0038Z000032k1DYQAY',
+    '0038Z000032lCRcQAM',
+    '0038Z000032moNQQAY',
+    '0038Z000032moNRQAY',
+    '0038Z000032moNSQAY',
+    '0038Z000032moNTQAY',
+    '0038Z000032moNUQAY',
+    '0038Z000032moNVQAY',
+    '0038Z000032moNWQAY',
+    '0038Z000032moNXQAY',
+    '0038Z000032moNYQAY',
+    '0038Z000032moNZQAY',
+    '0038Z000032moNaQAI',
+    '0038Z000032moNbQAI',
+    '0038Z000032moNcQAI',
+    '0038Z000032moNdQAI',
+    '0038Z000032moNeQAI',
+  ];
+
+  const token = await getSFToken();
+  axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  axiosInstance.defaults.headers.common['Content-Type'] = 'application/json';
+
+  const promises = contacts.map(async (id) => {
+    const contactUpdateUri = urls.SFOperationPrefix + '/Contact/' + id;
+    await axiosInstance.patch(contactUpdateUri, {
+      Home_Chef_Status__c: 'Invited to Orientation',
+    });
+    console.log();
+  });
+
+  await Promise.all(promises);
+};
+
+// module.exports = migrate;
+// updateContactsToInvited();
