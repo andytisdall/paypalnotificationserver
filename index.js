c8bee3f7850c9ccddcbb2fee71fbaa835f933a06
thatsongsucks <andrew.tisdall@gmail.com>
2023-01-28 17:45:37 -0800

    maybe finally putting this static thing to bed

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 const root = path.join(__dirname, 'client', 'build');
-app.use(express.static(root));
+app.use('/', express.static(root));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
