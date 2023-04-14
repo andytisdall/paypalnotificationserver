b1cfb01bdaa315645f5ac2bd2fe7795aab80c8ba
Andrew Tisdall <andrewtisdall@Andrews-MacBook-Pro.local>
2023-04-14 12:54:10 -0700

    home chef jobs get shifts from next 90 days

--- src/homeChef/routes/homeChefJobListing.ts
+++ src/homeChef/routes/homeChefJobListing.ts
@@ -93,9 +93,9 @@ const getJobs = async (id: string) => {
 };
 
 const getShifts = async (id: string) => {
-  const ThirtyDaysFromNow = moment().add(30, 'day').format();
+  const ninetyDaysFromNow = moment().add(90, 'days').format();
 
-  const query = `SELECT Id, GW_Volunteers__Start_Date_Time__c, GW_Volunteers__Number_of_Volunteers_Still_Needed__c from GW_Volunteers__Volunteer_Shift__c WHERE GW_Volunteers__Volunteer_Job__c = '${id}' AND GW_Volunteers__Start_Date_time__c > TODAY AND  GW_Volunteers__Start_Date_time__c <= ${ThirtyDaysFromNow}`;
+  const query = `SELECT Id, GW_Volunteers__Start_Date_Time__c, GW_Volunteers__Number_of_Volunteers_Still_Needed__c from GW_Volunteers__Volunteer_Shift__c WHERE GW_Volunteers__Volunteer_Job__c = '${id}' AND GW_Volunteers__Start_Date_time__c > TODAY AND  GW_Volunteers__Start_Date_time__c <= ${ninetyDaysFromNow}`;
 
   const shiftQueryUri = urls.SFQueryPrefix + encodeURIComponent(query);
 
