8dd6a6dc2e3c58566fb04d69e91fbc85075e2a6b
Andrew Tisdall <andrewtisdall@Andrews-MacBook-Pro.local>
2023-05-25 12:27:51 -0700

    changhe home chef contract template

--- src/docusign/createEnvelope.ts
+++ src/docusign/createEnvelope.ts
@@ -4,7 +4,7 @@ const prodTemplates: Record<string, string> = {
   W9: '9fc27ab6-ceb6-471f-a7a4-bc875cd06942',
   RC: '44e64a6a-a0a1-4c5a-87de-bb62d5f28b1e',
   DD: 'ec16dfbd-9b80-4a0a-858f-ac5cb404df70',
-  HC: '1b7dd9f3-cf35-4f6c-b667-69b9fd670b9e',
+  HC: '3c76d961-cce2-481b-a1ec-6b8377869622',
 };
 
 const devTemplates: Record<string, string> = {
@@ -30,13 +30,6 @@ export default ({
   signerClientId,
   doc,
 }: CreateEnvelopeArgs) => {
-  // const document = createDocument(doc);
-
-  // change account type to doc name and find the doc that way
-  // refactor tab creation into function that can output the different docs
-  // also needs to output email subject
-  // might as well merge that with mapAccountToFiles
-
   // Create a signer recipient to sign the document, identified by name and email
   // We set the clientUserId to enable embedded signing for the recipient
   // We're setting the parameters via the object creation
@@ -45,7 +38,6 @@ export default ({
     name: signerName,
     clientUserId: signerClientId,
     roleName: 'Signer',
-    // tabs: document.tabs,
   };
 
   // Add the recipient to the envelope object
@@ -57,86 +49,10 @@ export default ({
   // The order in the docs array determines the order in the envelope
   let env: EnvelopeDefinition = {
     emailSubject: 'CK: Complete this Document',
-    // documents: [document.document],
     status: 'sent',
     templateId: templates[doc],
     templateRoles: [signer],
-    // recipients,
   };
 
   return env;
 };
-
-// const mapDocNameToFiles: Record<string, Record<string, string>> = {
-//   restaurantContract: {
-//     file: 'World_Wide_Corp_lorem.pdf',
-//     name: 'Restaurant Contact',
-//     id: '1',
-//   },
-//   volunteerAgreement: {
-//     file: 'volunteer_agreement.pdf',
-//     name: 'Volunteer Agreement',
-//     id: '2',
-//   },
-// };
-
-// interface DocInfo {
-//   tabs: Tabs;
-//   emailSubject: string;
-//   document: Document;
-// }
-
-// const createDocument = (docName: string): DocInfo => {
-//   const fileName = path.resolve(__dirname, 'contracts', docName);
-//   // read file from a local directory
-//   // The read could raise an exception if the file is not available!
-//   let docPdfBytes: Buffer;
-//   try {
-//     docPdfBytes = fs.readFileSync(fileName);
-//   } catch {
-//     throw Error('Could not find contract on CK server');
-//   }
-
-//   // add the documents
-//   const doc: Document = {
-//     documentBase64: Buffer.from(docPdfBytes).toString('base64'),
-//     name: mapDocNameToFiles[docName].name, // can be different from actual file name
-//     fileExtension: 'pdf',
-//     documentId: mapDocNameToFiles[docName].id,
-//   };
-
-//   // Create signHere fields (also known as tabs) on the documents,
-//   // We're using anchor (autoPlace) positioning
-//   //
-//   // The DocuSign platform seaches throughout your envelope's
-//   // documents for matching anchor strings.
-//   let signTab: SignHere = {
-//     anchorString: '/sn1/',
-//   };
-
-//   let dateTab: DateSigned = {
-//     anchorString: '/dt1/',
-//   };
-
-//   let nameTab: FullName = {
-//     anchorString: '/fn1/',
-//   };
-
-//   let textTab: Text = {
-//     anchorString: '/txt1/',
-//   };
-
-//   // Tabs are set per recipient / signer
-//   let tabs: Tabs = {
-//     signHereTabs: [signTab],
-//     dateSignedTabs: [dateTab],
-//     fullNameTabs: [nameTab],
-//     textTabs: [textTab],
-//   };
-
-//   return {
-//     document: doc,
-//     emailSubject: mapDocNameToFiles[docName].subject,
-//     tabs,
-//   };
-// };
